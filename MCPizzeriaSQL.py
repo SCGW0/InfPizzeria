9504315a72d75391042a1c4ba363f67acb42c298
MVB <maarenvanbeek01@gmail.com>
2026-04-07 09:24:47 +0200

    tabel afdrukken

--- MCPizzeriaSQL.py
+++ MCPizzeriaSQL.py
@@ -15,7 +15,20 @@ with sqlite3.connect("MCPizzeria.db") as db:
 
 
 ### ---------  Functie definities  -----------------
+def maakTabellenAan():
+    # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tbl_pizzas(
+    gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
+    gerechtNaam TEXT NOT NULL,
+    gerechtPrijs REAL NOT NULL);""")
+    print("Tabel 'tbl_pizzas' a")
 
+def printTabel(tabel_naam):
+ cursor.execute("SELECT * FROM " + tabel_naam) #SQL om ALLE gegevens te halen
+ opgehaalde_gegevens = cursor.fetchall() #sla gegevens op in een variabele
+ print("Tabel " + tabel_naam + ":", opgehaalde_gegevens) #druk gegev
 
 ### --------- Hoofdprogramma  ---------------
-
+maakTabellenAan()
+printTabel('tbl_pizzas')
